2699564d4d773854559f4a1e756a5243ceecb70c
Dushan Aluvihare <aluviharedushan@gmail.com>
2026-03-24 16:04:32 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# ======================================================
+# LEARNING PYTHON : Functions & Recursions
+# ======================================================
+
+
+# 1. BASIC FUNCTION
+# A function taes inouts (parameters) and returns an output.
+
+def greet(name):
+    """Return a greeting message."""
+    return f"Hello, {name}! Welcome to Python."
+print (greet("Anuradha"))
